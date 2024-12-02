1c664aa68b7e591faba80418d6c20b555005579c
mohammed iqbal v <24mcam04@meaec.edu.in>
2024-12-02 15:10:19 +0530

    Create votevalidation.py

---
+++ votevalidation.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print(f"{name}, you are eligible to vote.")
+else:
+    print(f"{name}, you are not eligible to vote.")
